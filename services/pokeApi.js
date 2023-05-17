f237c7c69a2be383c2d2592e9ec6149f563ecde7
facucz <facucz@users.noreply.github.com>
2023-05-17 15:35:18 +0000

    deploy: 1b59a0c2161349ffae56d6d91c30f16e2ee01246

--- services/pokeApi.js
+++
@@ -1,7 +0,0 @@
-import { getData } from "../utils/axiosUtils.js";
-
-const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
-
-export async function getPokemonByName(pokemonName) {
-    return getData(baseUrl + `${pokemonName}`);
-}
